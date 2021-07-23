07af9afb585881a5810f3f238419266f1e7a9da8
prodigy803 <pushkaraj803@gmail.com>
2021-07-24 01:21:24 +0530

    Basic Working of AutoYInvoice.py

--- AutoYInvoice.py
+++ AutoYInvoice.py
@@ -1,13 +1,33 @@
+import os
+import pdftotext
+import glob
+
 class AutoYInvoice:
     def __init__(self):
         self.counter = 0
 
-    def get_templates(self):
+    def get_templates(self,templates_directory = ""):
         pass
 
-    def get_invoices(self):
-        pass
+    def process_invoices(self,invoices_directory = ""):
+
+        for invoice_pdf in glob.glob(invoices_directory+'/*.pdf'):
+            print(invoice_pdf)
+            with open(invoice_pdf, "rb") as f:
+                pdf = pdftotext.PDF(f)
+                for page in pdf:
+                    print(page)
+
+        
+if __name__ == '__main__':
+    print('Please enter the path to the invoices')
+    input1 = str(input())
+
+    print('Please enter the path to the templates')
+    input2 = str(input())
+
+    autoyinvoice_instance = AutoYInvoice()
+
+    autoyinvoice_instance.process_invoices(invoices_directory = input1)
 
     
-if __name__ == '__main__':
-    input1 = str(input())
