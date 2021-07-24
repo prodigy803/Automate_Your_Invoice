25e5f1c961bcb372d9a1a17ea59b7d5061eccbf3
prodigy803 <pushkaraj803@gmail.com>
2021-07-24 19:00:44 +0530

    main_script_edited

--- AutoYInvoice.py
+++ AutoYInvoice.py
@@ -3,20 +3,60 @@ import pdftotext
 import glob
 
 class AutoYInvoice:
-    def __init__(self):
-        self.counter = 0
 
-    def get_templates(self,templates_directory = ""):
-        pass
+    def __init__(self):
+        self.to_be_deleted_later_txt = []
+        self.rule_base = {}
+
+    def process_templates(self,templates_directory = ""):
+        template_txts = glob.glob(templates_directory+'/*.txt')
+        need_to_classify = False
+        if len(template_txts) > 1:
+            need_to_classify = True
+        
+        for template_txt in template_txts:
+            with open(template_txt, "r+") as f:
+                lines = f.readlines()
+            f.close()
+
+            if need_to_classify:
+                if not any(["Classification-Rules-Over-here" in x for x in lines]):
+                    print("Please mention the classification words and mention Classification-Rules-Over-here in the template files")
+                    return "Error-Please Enter the classification tags"
+
+                else:
+                    tags = ["Classification-Rules" in x for x in lines]
+                    tags2 = [i for i,x in enumerate(tags) if x]
+
+                    if len(tags2)>2:
+                        print("Please check whether 'Classification-Rules' is coming only twice in the entire template")
+                        return "Error-Correct your template"
+
+                    elif len(tags2) == 2:
+                        lines_v2 = [x.split('-|-') for x in lines[tags2[0]+1:tags2[1]]]
+                        
 
     def process_invoices(self,invoices_directory = ""):
 
-        for invoice_pdf in glob.glob(invoices_directory+'/*.pdf'):
+        ## OS.walk will be integrated later.
+        invoice_pdfs = glob.glob(invoices_directory+'/*.pdf')
+        
+        for invoice_pdf in invoice_pdfs:
             print(invoice_pdf)
+
+            text_file = open(invoice_pdf.replace('pdf','txt'), "w")
+            
+            self.to_be_deleted_later_txt.append(invoice_pdf.replace('pdf','txt'))
+            
+            
+
             with open(invoice_pdf, "rb") as f:
                 pdf = pdftotext.PDF(f)
                 for page in pdf:
                     print(page)
+                    text_file.write(page)
+
+            text_file.close()
 
         
 if __name__ == '__main__':
@@ -28,6 +68,7 @@ if __name__ == '__main__':
 
     autoyinvoice_instance = AutoYInvoice()
 
-    autoyinvoice_instance.process_invoices(invoices_directory = input1)
+    autoyinvoice_instance.process_invoices(invoices_directory = '/Users/pushkarajjoshi/Desktop/Projects/Automate_Your_Invoice/Demo/Demo_1/Invoices')
 
-    
+    autoyinvoice_instance.process_templates(templates_directory = '/Users/pushkarajjoshi/Desktop/Projects/Automate_Your_Invoice/Demo/Demo_1/Templates')
+
