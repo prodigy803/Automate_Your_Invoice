acb7835c8e95b73baaa83637d665901fe74995c4
prodigy803 <pushkaraj803@gmail.com>
2021-07-25 02:42:55 +0530

    Template Matching Completed

--- AutoYInvoice.py
+++ AutoYInvoice.py
@@ -3,6 +3,7 @@ import pdftotext
 import glob
 import pprint
 import yaml
+import pandas as pd
 
 class AutoYInvoice:
 
@@ -18,7 +19,7 @@ class AutoYInvoice:
 
         template_txts = glob.glob(templates_directory+'/*.txt')
 
-        print(template_txts)
+        # print(template_txts)
         
         need_to_classify = False
 
@@ -36,29 +37,46 @@ class AutoYInvoice:
                 lines = f.readlines()
             f.close()
 
+            lines = [x.replace('\n','') for x in lines]
+
             if need_to_classify:
                 if not any(["Classification-Rules-Over-here" in x for x in lines]):
                     print("Please mention the classification words and mention Classification-Rules-Over-here in the template files")
                     return "Error-Please Enter the classification tags"
 
                 else:
-                    tags = ["Classification-Rules" in x for x in lines]
-                    tags2 = [i for i,x in enumerate(tags) if x]
+                    tags_classification_rules = ["Classification-Rules" in x for x in lines]
+                    tags_classification_rules_2 = [i for i,x in enumerate(tags_classification_rules) if x]
 
 
-                    if len(tags2)>2:
+                    if len(tags_classification_rules_2)>2:
                         print("Please check whether 'Classification-Rules' is coming only twice in the entire template")
                         return "Error-Correct your template"
 
-                    elif len(tags2) == 2:
-                        lines_v2 = [x.split('-|-') for x in lines[tags2[0]+1:tags2[1]]]
+                    elif len(tags_classification_rules_2) == 2:
+                        lines_v2 = [x.split('-|-') for x in lines[tags_classification_rules_2[0]+1:tags_classification_rules_2[1]]]
+                    
+                    for line_v2 in lines_v2:
 
-                        for line_v2 in lines_v2:
-                            self.rule_base[template_txt][line_v2[0]] = {'keyword':line_v2[1],'Same_Line':line_v2[2],'Sequence':line_v2[3].split(',')}
-                        
-                        
+                        self.rule_base[template_txt][line_v2[0]] = {'keyword':line_v2[1],'Same_Line':line_v2[2],'Sequence':line_v2[3].split(',')}
+                    
+                    tags_body_rules = ["Body-Rules" in x for x in lines]
+                    tags_body_rules_2 = [i for i,x in enumerate(tags_body_rules) if x]
+
+                    if len(tags_body_rules_2)>2:
+                        print("Please check whether 'Classification-Rules' is coming only twice in the entire template")
+                        return "Error-Correct your template"
+
+                    elif len(tags_body_rules_2) == 2:
+                        lines_v2 = [x.split('-|-') for x in lines[tags_body_rules_2[0]+1:tags_body_rules_2[1]]]
+
+                    # print(lines_v2)
+                    for line_v2 in lines_v2:
+                        self.rule_base[template_txt][line_v2[0]] = {'keyword':line_v2[1],'Position':line_v2[2],'Type':line_v2[3],'Free Floating':line_v2[4],'Delimiter':line_v2[5]}
+
             with open(template_txt.replace('txt','yml'), 'w') as outfile:
                 yaml.dump(self.rule_base[template_txt], outfile, default_flow_style=False)
+
 
     def process_invoices(self,invoices_directory = ""):
         self.invoices_directory = invoices_directory
@@ -66,7 +84,7 @@ class AutoYInvoice:
         ## OS.walk will be integrated later.
         invoice_pdfs = glob.glob(invoices_directory+'/*.pdf')
 
-        print(invoice_pdfs)
+        # print(invoice_pdfs)
         self.to_be_deleted_later_invoices_txt = [x.replace('.pdf','.txt') for x in invoice_pdfs]
 
         for invoice_pdf in invoice_pdfs:
@@ -96,6 +114,40 @@ class AutoYInvoice:
 
         for file in self.to_be_deleted_later_templates_yaml:
             os.remove(file)
+
+    def extract_data(self):
+        templates = []
+
+        for file in self.to_be_deleted_later_templates_yaml:
+            with open(file, 'r') as stream:
+                try:
+                    templates.append(yaml.safe_load(stream))
+                except yaml.YAMLError as exc:
+                    print(exc)
+
+        classification_keys = []
+        
+        matching = {}
+
+        for template in templates:
+            for key in template.keys():
+                if "Keyword" in key:
+                    classification_keys.append(template[key]['keyword'])
+
+        for invoice in self.to_be_deleted_later_invoices_txt:
+
+            with open(invoice, "r+") as f:
+                lines = f.readlines()
+            
+            f.close()
+
+            if any([x in " ".join(lines) for x in classification_keys]):
+                for key in classification_keys:
+                    if key in " ".join(lines):
+                        matching[key] = " ".join(lines)
+                        break
+            
+            print('\n----------------\n')
         
 if __name__ == '__main__':
     print('Please enter the path to the invoices')
@@ -121,6 +173,8 @@ if __name__ == '__main__':
         print('There was some problem processing the , are you sure you are following the guidelines?')
         print('Please note the use of @---------------Body-Rules-Rules-Over-here---------------------- in the template?')
 
-    autoyinvoice_instance.process_yaml_file()
+    dataframe = autoyinvoice_instance.extract_data()
+
+    # autoyinvoice_instance.process_yaml_file()
 
     autoyinvoice_instance.delete_all_files()
